92399a0673c320dda380e9c97568d1def366a75b
siriratk <siriratk@amazon.com>
2019-03-29 10:49:28 -0400

    Add code

--- xmlfilevalidator.Tests/FunctionTest.cs
+++ xmlfilevalidator.Tests/FunctionTest.cs
@@ -8,21 +8,58 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
 using xmlfilevalidator;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Util;
+using Amazon.S3.Model;
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.Extensions.Configuration;
 
 namespace xmlfilevalidator.Tests
 {
     public class FunctionTest
     {
+        IConfiguration Configuration { get; set; }
+
         [Fact]
-        public void TestToUpperFunction()
+        public async Task IntegrationTest()
         {
 
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
+            
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
 
-            Assert.Equal("HELLO WORLD", upperCase);
+            var options = Configuration.GetAWSOptions();
+            IAmazonS3 s3client = options.CreateServiceClient<IAmazonS3>();
+            //IAmazonS3 s3client = new AmazonS3Client(RegionEndpoint.USEast1);
+            var bucketName = "siri-lambda-test";
+            var schemaName = "books.xsd";
+            var checktype = "False";
+            var fileNameGood = "booksSchemaPass.xml";
+            var fileNameFail = "booksSchemaFail.xml";
+            var s3Event = new S3Event
+            {
+                Records = new List<S3EventNotification.S3EventNotificationRecord>
+                    {
+                        new S3EventNotification.S3EventNotificationRecord
+                        {
+                            S3 = new S3EventNotification.S3Entity
+                            {
+                                Bucket = new S3EventNotification.S3BucketEntity {Name = "siri-lambda-test" },
+                                Object = new S3EventNotification.S3ObjectEntity {Key = fileNameGood }
+                            }
+                        }
+                    }
+            };
+
+            var function = new Function(s3client, bucketName, schemaName);
+            var context = new TestLambdaContext();
+            var result = await function.FunctionHandler(s3Event, context);
+                
+
+
+            Assert.Equal("good", result);
         }
     }
 }
--- xmlfilevalidator/Function.cs
+++ xmlfilevalidator/Function.cs
@@ -23,16 +23,19 @@ namespace xmlfilevalidator
 
         public string stxsd { get; set; }
         public string Schema_Target { get; set; }
-        public string schemaName { get; set; }
-        public string bucketName { get; set; }
+        public string SchemaName { get; set; }
+        public string BucketName { get; set; }
 
         public Function()
         {
             S3Client = new AmazonS3Client();
-            bucketName = System.Environment.GetEnvironmentVariable("BUCKET_NAME");
-            schemaName = System.Environment.GetEnvironmentVariable("SCHEMA_FILENAME");
-            Schema_Target = System.Environment.GetEnvironmentVariable("SCHEMA");
-            stxsd = GetObject(bucketName, schemaName).Result;
+            var buckName = System.Environment.GetEnvironmentVariable("BUCKET_NAME");
+            var schemaName = System.Environment.GetEnvironmentVariable("SCHEMA_FILENAME");
+            var schema_Target = System.Environment.GetEnvironmentVariable("SCHEMA");
+            BucketName = string.IsNullOrEmpty(buckName) ? "siri-lambda-test" : buckName;
+            SchemaName = string.IsNullOrEmpty(schemaName) ? "books.xsd" : schemaName;
+            Schema_Target = string.IsNullOrEmpty(schema_Target) ? "urn:books" : schema_Target;
+            stxsd = GetObject(BucketName, SchemaName).Result;
         }
 
         public Function(IAmazonS3 s3Client, string bucketName, string schemaName)
@@ -59,17 +62,23 @@ namespace xmlfilevalidator
                 MemoryStream stream = new MemoryStream(byteArray);
                 XmlReader xmlReaderS3Object = XmlReader.Create(stream);
                 context.Logger.LogLine("Validating " + s3Event.Object.Key);
-                context.Logger.LogLine("Schema File Name: " + schemaName);
+                context.Logger.LogLine("Schema File Name: " + SchemaName);
                 settings.Schemas.Add(Schema_Target, curFile);
                 settings.CheckCharacters = true;
                 settings.ValidationType = ValidationType.Schema;
-                settings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
-                XmlReader reader = XmlReader.Create(xmlReaderS3Object, settings);
-                while (reader.Read()) { }
-
-            }
-
-            
+                //settings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
+                try
+                {
+                    XmlReader reader = XmlReader.Create(xmlReaderS3Object, settings);
+                    while (reader.Read()) { }
+                    return "good";
+                }
+                catch (Exception e)
+                {
+                    context.Logger.LogLine(e.Message);
+                    return "bad";
+                }
+            } 
         }
 
         public async Task<string> GetObject(string bucket, string key)
@@ -86,6 +95,7 @@ namespace xmlfilevalidator
         {
             if (e.Severity == XmlSeverityType.Warning)
             {
+                
                 Console.Write("WARNING: ");
                 Console.WriteLine(e.Message);
             }
